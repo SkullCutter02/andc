294072a60a771e99a47de1f4d204ba153771337a
Alvin Ng <ngalvin2004@gmail.com>
2021-09-13 19:55:59 +0800

    whole lotta changes

--- components/Nav.js
+++ components/Nav.js
@@ -1,40 +1,12 @@
-class Nav extends HTMLElement {
-  constructor() {
-    super();
-  }
+let lastScroll = 0;
+navbar = document.getElementById("navbar");
+window.addEventListener("scroll", () => {
+  const scroll = window.pageYOffset;
 
-  static get observedAttributes() {
-    return ["tab"];
+  if (scroll > lastScroll) {
+    navbar.style.top = "-170px";
+  } else {
+    navbar.style.top = "0";
   }
-
-  attributeChangedCallback(attrName, oldVal, newVal) {
-    if (oldVal !== newVal && attrName === "tab")
-      this.innerHTML = `
-        <style>
-          .tab${newVal ? "-" + newVal : ""} a {
-            color: var(--primaryColor) !important;
-          }
-          
-          .tab${newVal ? "-" + newVal : ""}::after {
-            opacity: 100% !important;
-          }
-        </style>
-      
-        <nav>
-          <div class="logo">
-            <img src="../public/logo.png" alt="Logo" />
-          </div>
-          <ul>
-            <li class="tab-1">
-              <a href="../index.html">Home</a>
-            </li>
-            <li class="tab-2">
-              <a href="#">About Us</a>
-            </li>
-          </ul>
-        </nav>
-      `;
-  }
-}
-
-customElements.define("nav-component", Nav);
+  lastScroll = scroll;
+});
