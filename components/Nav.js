08a186124c9336a0e68806d7d363dc91aa97b517
Alvin Ng <ngalvin2004@gmail.com>
2021-09-15 15:54:19 +0800

    whole lotta changes

--- components/Nav.js
+++ components/Nav.js
@@ -1,12 +1,14 @@
-let lastScroll = 0;
 navbar = document.getElementById("navbar");
+logo = document.querySelector(".logo");
+
 window.addEventListener("scroll", () => {
   const scroll = window.pageYOffset;
 
-  if (scroll > lastScroll) {
-    navbar.style.top = "-170px";
+  if (scroll > 0) {
+    navbar.style.top = "-85px";
+    logo.style.opacity = "0";
   } else {
     navbar.style.top = "0";
+    logo.style.opacity = "1";
   }
-  lastScroll = scroll;
 });
